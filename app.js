e3979143250d871739752dbc405b14feaf229310
SmetaDe <smeta.deba@jjay.cuny.edu>
2023-07-25 10:17:01 -0400

    project3 part 2

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.get("/", (req, res) => {
   res.send(html);
 });
 
+
 // Define the Pokemon details route
 app.get("/pokemon/:id", (req, res) => {
   const pokemon = pokeBank.find(req.params.id);
@@ -39,6 +40,45 @@ app.get("/pokemon/:id", (req, res) => {
     throw new Error("Not Found");
   }
 });
+
+// Part 6 adding routes
+const Pokemon = require("./models/Pokemon");
+
+app.get("/pokemon", async (req, res) => {
+  const pokemon = await Pokemon.findAll();
+  res.json(pokemon);
+});
+app.get("/pokemon/:id", async (req, res) => {
+  const pokemon = await Pokemon.findByPk(req.params.id);
+  if (pokemon) {
+    res.json(pokemon);
+  } else {
+    res.status(404).send("Pokemon not found");
+  }
+});
+app.post("/pokemon", async (req, res) => {
+  const newPokemon = await Pokemon.create(req.body);
+  res.json(newPokemon);
+});
+app.put("/pokemon/:id", async (req, res) => {
+  const pokemon = await Pokemon.findByPk(req.params.id);
+  if (pokemon) {
+    await pokemon.update(req.body);
+    res.json(pokemon);
+  } else {
+    res.status(404).send("Pokemon not found");
+  }
+});
+app.delete("/pokemon/:id", async (req, res) => {
+  const pokemon = await Pokemon.findByPk(req.params.id);
+  if (pokemon) {
+    await pokemon.destroy();
+    res.status(204).send();
+  } else {
+    res.status(404).send("Pokemon not found");
+  }
+});
+
 
 // Start the server
 const PORT = 3000;
